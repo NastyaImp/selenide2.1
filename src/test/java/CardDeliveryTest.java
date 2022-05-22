fd877b834551a029f427d1a43a5ff321fde05ac0
nastyaImp <imperiian@yandex.ru>
2022-05-22 20:03:02 +0300

    test fix

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -17,14 +17,15 @@ public class CardDeliveryTest {
         Configuration.holdBrowserOpen=true;
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Уфа");
-        String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys. DELETE);
         $("[data-test-id=date] input").val(date);
         $("[name='name']").setValue("Анна-Мария Кузнецова");
         $("[name='phone']").setValue("+79232453487");
         $(By.className("checkbox__text")).click();
         $(By.className("button__content")).click();
-        $(By.className("notification__content")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(By.className("notification__content")).shouldHave(Condition.exactText("Встреча успешно забронирована на"+" "+ date),
+                Duration.ofSeconds(15));
 
 
 
